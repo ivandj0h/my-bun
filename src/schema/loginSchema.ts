485fe9916766e62da5514657611d4cbc519c672f
ivandjoh <ivandi.djoh@gmail.com>
2024-06-09 16:47:59 +0800

    code update

--- src/schema/loginSchema.ts
+++ src/schema/loginSchema.ts
@@ -4,7 +4,7 @@ export const schema = z.object({
     email: z.string().email(),
     password: z
         .string()
-        .min(8)
+        .min(2)
         .regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
             message:
                 "Minimum eight characters, at least one letter, one number and one special character",
