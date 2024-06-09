c6cc679d87f68e461e1c5e78346e06424f755e13
ivandjoh <ivandi.djoh@gmail.com>
2024-06-09 16:28:48 +0800

    add login

---
+++ src/schema/loginSchema.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+
+export const schema = z.object({
+    email: z.string().email(),
+    password: z
+        .string()
+        .min(8)
+        .regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
+            message:
+                "Minimum eight characters, at least one letter, one number and one special character",
+        }),
+});
